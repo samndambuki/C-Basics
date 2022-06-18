e5ddc85cc2e9df7766365afee5b3474f8c172c91
samndambuki <sam.ndambuki08@gmail.com>
2022-06-18 06:00:18 +0300

    Program Flow Commit

---
+++ Break and Continue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace BreakContinue{
+ class Program{
+  static void Main(string[] args){
+   int[] values = {15,7,12,23,41,28,9,17,36};
+   Console.WriteLine("Using Break and Continue");
+   foreach(int val in values){
+    //continues statement skips rest of loop entirely and jumps to nest iteration
+    if(val >=20 && val<=29)
+    continue;
+    //print value
+    Console.WriteLine($"val is currently{val}");
+    //Break statement stops the loop and exits
+    // if(val>=40){
+    //  break;
+    // }
+   }
+  }
+ }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Exceptions{
+ class Program{
+  static void Main(string[] args){
+   int x = 1002;
+   int y = 0;
+   int result;
+
+   try{
+    if(x>1000){
+     throw new ArgumentOutOfRangeException("x","X has to be 1000 or less");
+    }
+
+    result = x/y;
+   Console.WriteLine("The result is: {0}",result);
+   }
+   catch(DivideByZeroException e){
+    Console.WriteLine("Error!!");
+    Console.WriteLine(e.Message);
+   }
+   catch(ArgumentOutOfRangeException e){
+    Console.WriteLine("Sorry,1000 is the limit");
+    Console.WriteLine(e.Message);
+   }
+   finally{
+    Console.WriteLine("This code is always going to run");
+   }
+  }
+ }
+}
